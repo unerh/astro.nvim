51d65a48fbcc61a26833ba23fbe283ff21d75304
Hakan Uner <hakan.uner@xero.com>
2024-10-14 08:45:33 +1100

    override and ignore Deployer Recipe for yamlls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -14,22 +14,43 @@ return {
         "yamlls",
         -- add more arguments for adding more language servers
       },
-      -- enable debug logs for lsp
-      lspconfig = {
-        yamlls = {
-          settings = {
-            yaml = {
-              schemas = {
-                ["https://raw.githubusercontent.com/open-telemetry/opentelemetry-collector/refs/tags/cmd/mdatagen/v0.110.0/cmd/mdatagen/metadata-schema.yaml"] =
-                "/**/metadata.yaml",
-                -- add more schemas as needed
-              },
+    },
+  },
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+    dependencies = {
+      {
+        "AstroNvim/astrolsp",
+        optional = true,
+        ---@type AstroLSPOpts
+        opts = {
+          ---@diagnostic disable: missing-fields
+          config = {
+            yamlls = {
+              on_new_config = function(config)
+                config.settings.yaml.schemas = vim.tbl_deep_extend(
+                  "force",
+                  config.settings.yaml.schemas or {},
+                  require("schemastore").yaml.schemas() {
+                    ignore = { "Deployer Recipe" },
+                  }
+                )
+              end,
+              settings = { yaml = { schemaStore = { enable = false, url = "" } } },
             },
           },
         },
       },
     },
   },
+  -- {
+  --   "b0o/schemastore.nvim",
+  --   opts = {
+  --
+  --   }
+  --
+  -- },
   -- -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   -- {
   --   "jay-babu/mason-null-ls.nvim",
