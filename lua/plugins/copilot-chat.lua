b6d1e2b4efcbb9b1f050a775272ba5ffc90e3d47
Hakan Uner <hakan.uner@xero.com>
2024-10-14 08:45:11 +1100

    copy config from astrocommunity/editing-support/copilotchat-nvim/init.lua

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,92 +1,133 @@
-local prompts = {
-  -- Code-related prompts
-  Explain = "Please explain how the following code works.",
-  Review = "Please review the following code and provide suggestions for improvement.",
-  Tests = "Please explain how the selected code works, then generate unit tests for it.",
-  Refactor = "Please refactor the following code to improve its clarity and readability.",
-  FixCode = "Please fix the following code to make it work as intended.",
-  BetterNamings = "Please provide better names for the following variables and functions.",
-  Documentation = "Please provide documentation for the following code.",
-  SwaggerApiDocs = "Please provide documentation for the following API using Swagger.",
-  SwaggerJsDocs = "Please write JSDoc for the following API using Swagger.",
-  -- Text-related prompts
-  Summarize = "Please summarize the following text.",
-  Spelling = "Please correct any grammar and spelling errors in the following text.",
-  Wording = "Please improve the grammar and wording of the following text.",
-  Concise = "Please rewrite the following text to make it more concise.",
-}
-
 ---@type LazySpec
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
-  opts = {
-    show_help = "yes",
-    prompts = prompts,
-    debug = true,              -- Set to true to see the response from GitHub Copilot API. The log file will be in ~/.local/state/nvim/CopilotChat.nvim.log.
-    disable_extra_info = "no", -- Disable extra information (e.g.: system prompt, token count) in the response.
+  version = "^2",
+  cmd = {
+    "CopilotChat",
+    "CopilotChatOpen",
+    "CopilotChatClose",
+    "CopilotChatToggle",
+    "CopilotChatStop",
+    "CopilotChatReset",
+    "CopilotChatSave",
+    "CopilotChatLoad",
+    "CopilotChatDebugInfo",
+    "CopilotChatModels",
+    "CopilotChatExplain",
+    "CopilotChatReview",
+    "CopilotChatFix",
+    "CopilotChatOptimize",
+    "CopilotChatDocs",
+    "CopilotChatFixDiagnostic",
+    "CopilotChatCommit",
+    "CopilotChatCommitStaged",
   },
-  build = function()
-    vim.notify "Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim."
-  end,
-  event = "VeryLazy",
-  keys = {
-    -- Code-related commands
-    { "<leader>ace", "<cmd>CopilotChatExplain<cr>",        desc = "CopilotChat - Explain code" },
-    { "<leader>act", "<cmd>CopilotChatTests<cr>",          desc = "CopilotChat - Generate tests" },
-    { "<leader>acr", "<cmd>CopilotChatReview<cr>",         desc = "CopilotChat - Review code" },
-    { "<leader>acR", "<cmd>CopilotChatRefactor<cr>",       desc = "CopilotChat - Refactor code" },
-    { "<leader>acf", "<cmd>CopilotChatFixCode<cr>",        desc = "CopilotChat - Fix code" },
-    { "<leader>acb", "<cmd>CopilotChatBetterNamings<cr>",  desc = "CopilotChat - Better Name" },
-    { "<leader>acd", "<cmd>CopilotChatDocumentation<cr>",  desc = "CopilotChat - Add documentation for code" },
-    { "<leader>aca", "<cmd>CopilotChatSwaggerApiDocs<cr>", desc = "CopilotChat - Add Swagger API documentation" },
+  dependencies = {
+    { "zbirenbaum/copilot.lua" },
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
     {
-      "<leader>acA",
-      "<cmd>CopilotChatSwaggerJsDocs<cr>",
-      desc = "CopilotChat - Add Swagger API with Js Documentation",
-    },
-    -- Text related commands
-    { "<leader>acs", "<cmd>CopilotChatSummarize<cr>", desc = "CopilotChat - Summarize text" },
-    { "<leader>acS", "<cmd>CopilotChatSpelling<cr>",  desc = "CopilotChat - Correct spelling" },
-    { "<leader>acw", "<cmd>CopilotChatWording<cr>",   desc = "CopilotChat - Improve wording" },
-    { "<leader>acc", "<cmd>CopilotChatConcise<cr>",   desc = "CopilotChat - Make text concise" },
-    -- Chat with Copilot in visual mode
-    {
-      "<leader>acv",
-      ":CopilotChatVisual",
-      mode = "x",
-      desc = "CopilotChat - Open in vertical split",
-    },
-    {
-      "<leader>acx",
-      ":CopilotChatInPlace<cr>",
-      mode = "x",
-      desc = "CopilotChat - Run in-place code",
-    },
-    -- Custom input for CopilotChat
-    {
-      "<leader>aci",
-      function()
-        local input = vim.fn.input "Ask Copilot: "
-        if input ~= "" then vim.cmd("CopilotChat " .. input) end
-      end,
-      desc = "CopilotChat - Ask input",
-    },
-    -- Quick chat with Copilot
-    {
-      "<leader>acq",
-      function()
-        local input = vim.fn.input "Quick Chat: "
-        if input ~= "" then
-          -- Copy all the lines to the unnamed register
-          vim.cmd 'normal! ggVG"*y'
-          vim.cmd("CopilotChat " .. input)
+      "AstroNvim/astrocore",
+      ---@param opts AstroCoreOpts
+      opts = function(_, opts)
+        local maps = assert(opts.mappings)
+        local prefix = opts.options.g.copilot_chat_prefix or "<Leader>P"
+        local astroui = require "astroui"
+
+        maps.n[prefix] = { desc = astroui.get_icon("CopilotChat", 1, true) .. "CopilotChat" }
+        maps.v[prefix] = { desc = astroui.get_icon("CopilotChat", 1, true) .. "CopilotChat" }
+
+        maps.n[prefix .. "o"] = { ":CopilotChatOpen<CR>", desc = "Open Chat" }
+        maps.n[prefix .. "c"] = { ":CopilotChatClose<CR>", desc = "Close Chat" }
+        maps.n[prefix .. "t"] = { ":CopilotChatToggle<CR>", desc = "Toggle Chat" }
+        maps.n[prefix .. "r"] = { ":CopilotChatReset<CR>", desc = "Reset Chat" }
+        maps.n[prefix .. "s"] = { ":CopilotChatStop<CR>", desc = "Stop Chat" }
+
+        maps.n[prefix .. "S"] = {
+          function()
+            vim.ui.input({ prompt = "Save Chat: " }, function(input)
+              if input ~= nil and input ~= "" then require("CopilotChat").save(input) end
+            end)
+          end,
+          desc = "Save Chat",
+        }
+
+        maps.n[prefix .. "L"] = {
+          function()
+            local copilot_chat = require "CopilotChat"
+            local path = copilot_chat.config.history_path
+            local chats = require("plenary.scandir").scan_dir(path, { depth = 1, hidden = true })
+            -- Remove the path from the chat names and .json
+            for i, chat in ipairs(chats) do
+              chats[i] = chat:sub(#path + 2, -6)
+            end
+            vim.ui.select(chats, { prompt = "Load Chat: " }, function(selected)
+              if selected ~= nil and selected ~= "" then copilot_chat.load(selected) end
+            end)
+          end,
+          desc = "Load Chat",
+        }
+
+        -- Helper function to create mappings
+        local function create_mapping(action_type, selection_type)
+          return function()
+            require("CopilotChat.integrations.telescope").pick(require("CopilotChat.actions")[action_type] {
+              selection = require("CopilotChat.select")[selection_type],
+            })
+          end
         end
+
+        maps.n[prefix .. "p"] = {
+          create_mapping("prompt_actions", "buffer"),
+          desc = "Prompt actions",
+        }
+
+        maps.v[prefix .. "p"] = {
+          create_mapping("prompt_actions", "visual"),
+          desc = "Prompt actions",
+        }
+
+        maps.n[prefix .. "d"] = {
+          create_mapping("help_actions", "buffer"),
+          desc = "LSP Diagnostics actions",
+        }
+
+        maps.v[prefix .. "d"] = {
+          create_mapping("help_actions", "visual"),
+          desc = "LSP Diagnostics actions",
+        }
+
+        -- Quick Chat function
+        local function quick_chat(selection_type)
+          return function()
+            vim.ui.input({ prompt = "Quick Chat: " }, function(input)
+              if input ~= nil and input ~= "" then
+                require("CopilotChat").ask(input, { selection = require("CopilotChat.select")[selection_type] })
+              end
+            end)
+          end
+        end
+
+        maps.n[prefix .. "q"] = {
+          quick_chat "buffer",
+          desc = "Quick Chat",
+        }
+
+        maps.v[prefix .. "q"] = {
+          quick_chat "visual",
+          desc = "Quick Chat",
+        }
       end,
-      desc = "CopilotChat - Quick chat",
     },
-    -- Debug
-    { "<leader>acD", "<cmd>CopilotChatDebugInfo<cr>",     desc = "CopilotChat - Debug Info" },
-    -- Fix the issue with the diagnostic
-    { "<leader>acF", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
+    { "AstroNvim/astroui", opts = { icons = { CopilotChat = "" } } },
+  },
+  opts = {
+    window = {
+      layout = "float",
+      width = 74,               -- absolute width in columns
+      height = vim.o.lines - 4, -- absolute height in rows, subtract for command line and status line
+      row = 1,                  -- row position of the window, starting from the top
+      col = vim.o.columns - 74, -- column position of the window, aligned to the right
+    },
   },
 }
